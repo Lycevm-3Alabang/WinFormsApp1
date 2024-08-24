7dae5ebf7ed4967ff48603c11bd155cdbb45917c
Ninism <nin.alamo@outlook.com>
2024-08-25 07:35:07 +0800

    Add project files.

---
+++ MdiForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WinFormsApp1
+{
+    partial class MdiForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            altFileToolStripMenuItem = new ToolStripMenuItem();
+            addToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { altFileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(894, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // altFileToolStripMenuItem
+            // 
+            altFileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { addToolStripMenuItem });
+            altFileToolStripMenuItem.Name = "altFileToolStripMenuItem";
+            altFileToolStripMenuItem.Size = new Size(59, 20);
+            altFileToolStripMenuItem.Text = "&Actions";
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(180, 22);
+            addToolStripMenuItem.Text = "&View Items";
+            // 
+            // MdiForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(894, 602);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "MdiForm";
+            Text = "Coffee Shop - WinForms";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem altFileToolStripMenuItem;
+        private ToolStripMenuItem addToolStripMenuItem;
+    }
+}
---
+++ MdiForm.cs
@@ -0,0 +1,10 @@
+namespace WinFormsApp1
+{
+    public partial class MdiForm : Form
+    {
+        public MdiForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
